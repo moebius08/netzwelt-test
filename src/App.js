af886963a650ac1688e6ba29d5355b44e2492035
moebius08 <72979484+moebius08@users.noreply.github.com>
2023-08-19 21:47:40 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import {BrowserRouter,Routes,Route, HashRouter} from 'react-router-dom';
 import Homepage from './pages/homepage'
 import Login from './pages/login';
 import ProtectedRoutes from './components/ProtectedRoutes';
@@ -6,12 +6,12 @@ import PublicRoute from './components/PublicRoutes';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
         <Routes>
       <Route path='/' element={<ProtectedRoutes> <Homepage /> </ProtectedRoutes>} />
       <Route path='/account/login' element={<PublicRoute><Login /></PublicRoute>}/>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
     );
 }
 
