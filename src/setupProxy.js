638f6cc8d2f83483463240dc577e05451c3360ff
moebius08 <72979484+moebius08@users.noreply.github.com>
2023-08-19 08:22:03 +0800

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,14 +4,14 @@ module.exports = function(app) {
   app.use(
     '/Account',
     createProxyMiddleware({
-      target: `REACT_APP_URL`,
+      target: `${process.env.REACT_APP_URL}`,
       changeOrigin: true,
     })
   );
   app.use(
     '/Territories',
     createProxyMiddleware({
-      target: `REACT_APP_URL`,
+      target: `${process.env.REACT_APP_URL}`,
       changeOrigin: true,
     })
   );
